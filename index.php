503e5a2e4568bf36d1aa0ea3f8158cfe82330f57
Kushal <custkurajkumar@gmail.com>
2024-11-03 20:09:25 +0530

    Addition of Links

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#0000ff">
+	<title>Quick Links</title>
+	<!-- <link rel="icon" href="../images/favicon.ico" sizes="32x32" />
+	<link rel="icon" href="../images/favicon.ico" sizes="192x192" />
+	<link rel="apple-touch-icon" href="../images/favicon.ico" />
+	<meta name="msapplication-TileImage" content="../images/favicon.ico" /> -->
+</head>
+
+<body>
+    <section class="main-wrap">
+		<div class="container pt-4">
+			<div class="row">
+				<div class="col-md">
+					<h2>Links</h2>
+					<ul>
+						<li><a href="http://192.168.0.1/" target="_blank" rel="noopener noreferrer">TP Link / Internet</a></li>
+						<li><a href="https://docs.google.com/spreadsheets/d/17UB1i5n5Mx9adBI9vgInzI1nFvLS1e2y" target="_blank" rel="noopener noreferrer">All Reminder</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<style type="text/css">.disclaimer {display: none;}
+	html, body, div, span, applet, object, iframe,
+	h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+	a, abbr, acronym, address, big, cite, code,
+	del, dfn, em, img, ins, kbd, q, s, samp,
+	small, strike, strong, sub, sup, tt, var,
+	b, u, i, center,
+	dl, dt, dd, ol, ul, li,
+	fieldset, form, label, legend,
+	table, caption, tbody, tfoot, thead, tr, th, td,
+	article, aside, canvas, details, embed, 
+	figure, figcaption, footer, header, hgroup, 
+	menu, nav, output, ruby, section, summary,
+	time, mark, audio, video {
+		margin: 0;
+		padding: 0;
+		border: 0;
+		font-size: 100%;
+		font: inherit;
+		vertical-align: baseline;
+	}
+	/* HTML5 display-role reset for older browsers */
+	article, aside, details, figcaption, figure, 
+	footer, header, hgroup, menu, nav, section {
+		display: block;
+	}
+	*, body {
+		line-height: 20px;
+		font-family: Arial;
+		font-size: 14px;
+		letter-spacing: 0.6px;
+	}
+	html body > div, body > div {width: 0!important; height: 0!important; opacity: 0 !important; display: none !important; }
+	h1, h2, h3, h4 {font-weight: bold;} h1 { font-size: 24px; } h2 { font-size: 20px; } h3 { font-size: 18px; }
+	p {padding-bottom: 20px;}
+	ol, ul { list-style: none; }
+	blockquote, q { quotes: none; }
+	blockquote:before, blockquote:after,
+	q:before, q:after {
+		content: '';
+		content: none;
+	}
+	table {
+		border-collapse: collapse;
+		border-spacing: 0;
+	}
+	hr {margin-bottom: 20px;}
+	.main-wrap, .user-wrap {padding: 10px; box-sizing: border-box; width: 100%;} .fw-bold {font-weight: bold;} p span {font-size: 16px;}
+	b {font-weight: bold;}
+	ul {padding: 10px 0; padding-left: 15px; margin-top: 5px;}
+	ul li {margin-bottom: 10px; list-style: square; font-size: 16px;}
+	ul li a {transition: 0.2s all ease-in-out;}
+	ul li a:hover {text-decoration: double underline;}
+	.user-wrap { color: #fff; text-align: center; font-size: 12px; background: linear-gradient(45deg, #CD7F32, #391802); }
+	.user-wrap b { text-transform: uppercase; }
+	form.custform {text-align: center;}
+	.logout-btn { background-color: transparent; border: 2px solid #1A1A1A; border-radius: 15px; box-sizing: border-box; color: #3B3B3B; cursor: pointer; display: inline-block; padding: 5px 20px; text-align: center; text-decoration: none; transition: all 300ms cubic-bezier(.23, 1, 0.32, 1); font-weight: bold;}
+	.logout-btn:hover { color: #fff; background-color: #1A1A1A; box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+	transform: translateY(-2px); }
+	.row {
+		display: grid;
+		grid-template-columns: 50% 50%;
+		grid-gap: 0;
+		padding: 10px 0;
+	}
+	.row .col-md { border-bottom: 1px solid #ccc;}
+	.row:last-child .col-md { border-bottom: 0 none;}
+	.btn-wrap {text-align: center; clear: both; padding-bottom: 10px;}
+	@media only screen and (max-width: 620px) {
+		.row {display: block; grid-template-columns: 100%; padding-top: 0; padding-bottom: 0;}
+		.row:first-child {}
+		.row h2 {margin-top: 20px;}
+		.row .col-md, .row:last-child .col-md { border-bottom: 1px solid #ccc;}
+		.row:last-child .col-md:last-child { border-bottom: 0 none;}
+	}
+	</style>
+</body>
+</html>
